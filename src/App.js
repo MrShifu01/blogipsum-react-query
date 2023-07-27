d5c701310ae4f5bca82956119eb8aea4b8dd5d24
MrShifu01 <stander.christian.com>
2023-07-27 08:19:05 +0200

    react-query course

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { Posts } from './components/Posts';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Blog Ipsum</h1>
+        <Posts/>
+      </div>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
